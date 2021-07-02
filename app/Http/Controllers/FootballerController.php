6171fb7213a2ea2281b35be94ec6702684abd59d
Atakan <ati64.98.35@gmail.com>
2021-07-02 17:15:33 +0300

    Installed bootstrap

--- app/Http/Controllers/FootballerController.php
+++ app/Http/Controllers/FootballerController.php
@@ -9,21 +9,21 @@ class FootballerController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     *
      */
     public function index()
     {
-        //
+        return view("pages.main");
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     *
      */
     public function create()
     {
-        //
+
     }
 
     /**
