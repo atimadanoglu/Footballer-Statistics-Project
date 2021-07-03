091aaa7957a873c568a20b85d6069142528d5416
Atakan <ati64.98.35@gmail.com>
2021-07-03 20:45:13 +0300

    Implemented show blade view

--- resources/views/footballers/show.blade.php
+++ resources/views/footballers/show.blade.php
@@ -1,8 +1,20 @@
 @extends("layouts.default")
 
-@section("title", "Footballer")
+@section("title", $footballer["name"])
 
 @section("content")
 
+    <h1 class="text-center">Showing <strong>{{ $footballer["name"]}}</strong></h1>
+
+    <div class="jumbotron text-center">
+        <h2>{{ $footballer["name"] }}</h2>
+        <p>
+            <strong>{{__("Club Name:")}}</strong> {{ $footballer["clubName"] }}<br>
+            <strong>{{__("Age:")}}</strong> {{ $footballer["age"] }}<br>
+            <strong>{{__("Match Count:")}}</strong> {{ $footballer["matchCount"] }}<br>
+            <strong>{{__("Goal Count:")}}</strong> {{ $footballer["goalCount"] }}<br>
+            <strong>{{__("Assist Count:")}}</strong> {{$footballer["assistCount"]}}
+        </p>
+    </div>
 
 @endsection
